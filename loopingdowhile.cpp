6851cfc0a6c473fa2b675a84fd15a3e302dce1b7
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-07-21 10:59:39 +0700

    Fix formatting and add missing newline at end of file in loopingdowhile.cpp

--- loopingdowhile.cpp
+++ loopingdowhile.cpp
@@ -6,4 +6,10 @@ using namespace std;
 int main() {
     int x ;
     srand(time(0));
-   
+    //
+    cout << "PERULANGAN DO WHILE" << endl;
+    x = 1 + rand() % 10;
+    do {
+        cout << "TI,: Bilangan Acak = " << x << endl;
+        x = rand() % 10;
+    } while (x <= 5);
